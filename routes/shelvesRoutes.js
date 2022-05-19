064f123e5d49646814e6371a51f7a9eb209d77a4
JoshClopton <joshclopton@gmail.com>
2022-05-18 21:08:27 -0700

    add/update axios call for requesting shelves

--- routes/shelvesRoutes.js
+++ routes/shelvesRoutes.js
@@ -15,8 +15,38 @@ router.get("/", (req, res) => {
 	knex
 		.select("*")
 		.from("shelves")
-		.then((shelves) => {
-			res.status(200).json(shelves);
+		.then((data) => {
+			res.status(200).json(data);
+		})
+		.catch((err) => {
+			res.status(500).send("Error fetching shelves");
+		});
+});
+
+// Create a new post route
+router.post("/", (req, res) => {
+	// If user is not logged in, we don't allow them to create a new post
+	if (req.user === undefined)
+		return res.status(401).json({ message: "Unauthorized" });
+
+	// // Validate request body for required fields
+	// if (!req.body.title || !req.body.content) {
+	//   return res.status(400).json({ message: 'Missing post title or content fields' });
+	// }
+
+	// Insert new post into DB: user_id comes from session, title and content from a request body
+	knex("shelves")
+		.insert({
+			user: req.user.id,
+			title: req.body.title,
+			content: req.body.content,
+		})
+		.then((postId) => {
+			// Send newly created postId as a response
+			res.status(201).json({ newPostId: postId[0] });
+		})
+		.catch(() => {
+			res.status(500).json({ message: "Error creating a new post" });
 		});
 });
 
